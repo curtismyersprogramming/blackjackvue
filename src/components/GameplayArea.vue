554c047bb1de2ac00f9bf459205b96fdc804d45d
Curtis Myers <curtismyersprogramming@gmail.com>
2022-03-30 23:29:14 +0100

    Added player and dealer section

--- src/components/GameplayArea.vue
+++ src/components/GameplayArea.vue
@@ -1,5 +1,10 @@
 <template>
-    
+    <main class="game-area">
+        <section class="dealer-area">
+        </section>
+        <section class ="player-area">
+        </section>
+    </main>
 </template>
 
 <script>
@@ -9,5 +14,30 @@ export default {
 </script>
 
 <style scoped>
+    .game-area{
+        min-height: 88vh;
+        display: flex;
+        flex: 1;
+        flex-direction: column;
+        z-index: 50;
+        background-color: red;
+    }
 
+    .dealer-area {
+  
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: center;
+  align-items: center;
+  min-height: 12.4rem;
+  background-color: blueviolet;
+}
+.player-area {
+  flex: 1 0;
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: space-around;
+  align-items: center;
+  background-color: blue;
+}
 </style>
