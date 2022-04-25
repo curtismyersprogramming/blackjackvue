8ddb283bab071cdd5b8d7a4a8d2e6f7c35688a64
Curtis Myers <curtismyersprogramming@gmail.com>
2022-04-25 18:19:42 +0100

    added info area

--- src/components/GameplayArea.vue
+++ src/components/GameplayArea.vue
@@ -4,8 +4,22 @@
             <h1>Dealer</h1>
         </section>
         <section class ="player-area">
+            
             <h1>Player</h1>
+            
+            
         </section>
+        <div class="info-area">
+              
+                <div class="info-bar">
+                    <div class="grid-item">Dealers Hand</div>
+                    <div class="grid-item">Feedback</div>
+                    <div class="grid-item">Bank</div>
+                    <div class="grid-item">Count Values</div>
+                    <div class="grid-item">Players Hand</div>
+
+                    </div>
+                </div>
     </main>
 </template>
 
@@ -31,7 +45,7 @@ export default {
   flex-flow: row nowrap;
   justify-content: center;
   align-items: center;
-  min-height: 12.4rem;
+  min-height: 15.4rem;
   background-color: blueviolet;
 }
 .player-area {
@@ -41,5 +55,26 @@ export default {
   justify-content: space-around;
   align-items: center;
   background-color: blue;
+}
+
+.info-area{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 120px;
+}
+
+.info-bar{
+    display: grid;
+    justify-items: center;
+    align-items: center;
+    background-color: #111111;
+    width: 95%;
+    height: 85%;
+    border-radius: 15px;
+    grid-template-columns: auto auto auto auto auto;
+    color: white;
+    
+    
 }
 </style>
